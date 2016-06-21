49ad835ac67e6706ee9aa6feea6e9cbd45d90546
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-06-20 19:36:23 -0700

    updated origin controller

--- model/origin.js
+++ model/origin.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose');
 const originSchema = mongoose.Schema({
   country: {type: String, required: true}
   ,recMethod: {type: mongoose.Schema.ObjectId, required: true}
-  ,originId: {type: mongoose.Schema.ObjectId, required: true}
 });
 
 module.exports = mongoose.model('origin', originSchema);
