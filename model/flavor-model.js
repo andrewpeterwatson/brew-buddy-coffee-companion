ef0b6d2c6a732d23e68a62bd252d39e8ec1e10a6
FazioD <faziod@uw.edu>
2016-06-20 18:39:52 -0700

    adding array

--- model/flavor-model.js
+++ model/flavor-model.js
@@ -6,7 +6,7 @@ const flavorSchema = mongoose.Schema({
   title: { type: String, required: true}
   , flavor: {type: String, required: true}
   , name: { type: String, required: true}
-  , adjective: {type: Array, required: true}
+  , adjective: {type: [Array], required: true}
 });
 
 module.exports = mongoose.model('flavor', flavorSchema);
