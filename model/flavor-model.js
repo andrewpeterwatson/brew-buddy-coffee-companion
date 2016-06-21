730c98e788695f2f05bb64d5d7e695ce5d54754a
FazioD <faziod@uw.edu>
2016-06-20 18:37:59 -0700

    updating flavor-model

--- model/flavor-model.js
+++ model/flavor-model.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const flavorSchema = mongoose.Schema({
   title: { type: String, required: true}
   , flavor: {type: String, required: true}
-  , name: { String, required: true}
-  , adjective: {Array, required: true}
+  , name: { type: String, required: true}
+  , adjective: {type: Array, required: true}
 });
 
 module.exports = mongoose.model('flavor', flavorSchema);
