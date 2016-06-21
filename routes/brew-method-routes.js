fb8aebe1d90c1423acd013cb793d745f21e71c97
Andrew Watson <andrewpeterwatson@gmail.com>
2016-06-20 21:00:23 -0700

    created brew-method routes

---
+++ routes/brew-method-routes.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const debug = require('debug')('brewBuddy:brew-method-routes');
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+
+const httpErrors = require('http-errors');
+const parseBearerAuth = require('../lib/parse-bearer-auth');
+const brewMethodController = require('../controller/brew-method-controller');
+
+const brewMethodRouter = module.exports = new Router();
+
+brewMethodRouter.post('/method', parseBearerAuth, jsonParser, function(req, res, next) {
+  debug('POST /api/brewmethod');
+  brewMethodController.createBrewMethod(req.body)
+  .then(brewMethod => res.json(brewMethod))
+  .catch(next);
+});
+
+brewMethodRouter.get('/method', (req, res, next) => {
+  next(httpErrors(400, 'no ID provided'));
+});
+
+brewMethodRouter.get('/method/:id', parseBearerAuth, (req, res, next) => {
+  brewMethodController.fetchBrewMethod(req.params.id)
+  .then(brewMethod => {
+    if(!brewMethod) return next(httpErrors(404, 'not found'));
+  })
+  .catch(next);
+});
+
+brewMethodRouter.get('/method/all', parseBearerAuth, jsonParser, (req, res, next) => {
+  brewMethodController.fetchAllBrewMethods()
+  .then((brewMethod) => {
+    res.json(brewMethod);
+  })
+  .catch(next);
+});
+
+brewMethodRouter.put('/method/:id', parseBearerAuth, jsonParser, (req,res,next) => {
+  brewMethodController.updateBrewMethod(req.params.id, req.body)
+  .then(brewMethod => {
+    if(!brewMethod) return next(httpErrors(404, 'not found'));
+    res.json(brewMethod);
+  })
+  .catch(next);
+});
+
+brewMethodRouter.delete('/method/:id', parseBearerAuth, (req, res, next) => {
+  brewMethodController.removeAllBrewMethods(req.params.id)
+  .then(() => {
+    res.status(204).send();
+  })
+  .catch(next);
+});
