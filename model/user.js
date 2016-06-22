0874b2ebca08bc90a03dfbdc18d5fedbf7f743f2
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-06-21 20:15:48 -0700

    trying to fix auth

--- model/user.js
+++ model/user.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   username: {type: String, required: true, unique:true},
-  password: {type: String, required: true},
+  password: {type: String},
   findHash: {type: String, unique: true}
 });
 
