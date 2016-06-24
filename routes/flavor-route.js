21b1ad6abade986809952c9268167b78055912bd
FazioD <faziod@uw.edu>
2016-06-23 18:43:33 -0700

    fixing changes

--- routes/flavor-route.js
+++ routes/flavor-route.js
@@ -4,12 +4,12 @@ const Router = require('express').Router;
 const debug = require('debug')('brewbuddie:flavor-route');
 const jsonParser = require('body-parser').json();
 const flavorController = require('../controller/flavor-controller');
-// const parserBearerAuth = require('../lib/');//adding in file//
+const parserBearerAuth = require('../controller/lib/parse-bearer-auth');
 
 
 const flavorRouter = module.exports = new Router;
 
-flavorRouter.post('/flavor', jsonParser, function(req, res, next){
+flavorRouter.post('/flavor', jsonParser, parserBearerAuth, function(req, res, next){
   debug('post /flavor');
   flavorController.createFlavor(req.body)
   .then((flavor) => {
@@ -20,21 +20,21 @@ flavorRouter.post('/flavor', jsonParser, function(req, res, next){
 });
 
 
-flavorRouter.get('/flavor/:id', function(req, res, next){
+flavorRouter.get('/flavor/:id',parserBearerAuth, function(req, res, next){
   debug('get /flavor/:id');
   flavorController.fetchFlavor(req.params.id)
   .then(flavor => res.json(flavor))
   .catch(next);
 });
 
-flavorRouter.put('/flavor/:id', jsonParser, function(req, res, next){
+flavorRouter.put('/flavor/:id', jsonParser, parserBearerAuth, function(req, res, next){
   debug('put /flavor/:id');
   flavorController.updateFlavor(req.params.id, req.body)
   .then(flavor => res.json(flavor))
   .catch(next);
 });
 
-flavorRouter.delete('/flavor/:id', function(req, res, next) {
+flavorRouter.delete('/flavor/:id',parserBearerAuth, function(req, res, next) {
   debug('delete /flavor/:id');
   flavorController.deleteFlavor(req.params.id)
   .then(() => res.status(204).end())
